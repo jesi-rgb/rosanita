a1152d06b184a90caedb151a43ae2527c396f435
Jesús Rascón <blograso@gmail.com>
2022-12-26 00:27:33 +0100

    artist is kinda animated jeje

---
+++ src/Components/MovingWord.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let word;
+	let toggle = false;
+	$: letterSpacing = toggle ? 1 : 3;
+	$: fontWeight = toggle ? 500 : 700;
+	$: console.log(letterSpacing);
+</script>
+
+{#key word}
+	<button
+		style="letter-spacing:{letterSpacing}px;font-weight:{fontWeight}"
+		on:mouseover={() => {
+			console.log(toggle);
+			toggle = !toggle;
+		}}>{word}</button
+	>
+{/key}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
-<div class="text-9xl font-bold tracking-tight text-red-500">Rosa <br /> nita</div>
+<script>
+	import MovingWord from '../Components/MovingWord.svelte';
+	import { scale } from 'svelte/transition';
+</script>
 
-<p class="my-10 text-lg">
-	I am a young artist experimenting and having fun. My main sources of inspiration are nature and
-	home. Thanks for stopping by and making a place for me &lt;3
-</p>
+<div class="my-64">
+	<div in:scale={{ start: 0 }} class="text-9xl font-bold tracking-tight text-red-500">
+		Rosa <br /> nita
+	</div>
+
+	<p class="my-10 text-lg max-w-xs">
+		I am a young <MovingWord word={'artist'} /> experimenting and having fun. My main sources of inspiration
+		are nature and home. Thanks for stopping by and making a place for me &lt;3
+	</p>
+	<p class="my-40 text-lg max-w-lg">
+		I am a young artist experimenting and having fun. My main sources of inspiration are nature and
+		home. Thanks for stopping by and making a place for me &lt;3
+	</p>
+</div>
