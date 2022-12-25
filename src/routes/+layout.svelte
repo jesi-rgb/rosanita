f4241f5fa8555d0297838edb3afd533a656e5105
Jesús Rascón <blograso@gmail.com>
2022-12-25 22:07:11 +0100

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<main
+	class="mx-auto max-w-xs xl:max-w-2xl 2xl:max-w-5xl font-body my-10 selection:bg-red-500 selection:text-red-50"
+>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="text-9xl font-bold tracking-tight text-red-500">Rosa <br /> nita</div>
+
+<p class="my-10 text-lg">
+	I am a young artist experimenting and having fun. My main sources of inspiration are nature and
+	home. Thanks for stopping by and making a place for me &lt;3
+</p>
