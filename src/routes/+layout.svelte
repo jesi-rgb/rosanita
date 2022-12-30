7d168e66dfcdcb7566d1a582635e54b41b030e09
Jesús Enrique Rascón <blograso@gmail.com>
2022-12-30 23:58:45 +0100

    add blur and width consistency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main
-	class="mx-auto max-w-sm md:max-w-2xl xl:max-w-5xl 2xl:max-w-5xl font-body text-teal-900 selection:bg-red-500 selection:text-red-50"
+	class="font-body text-teal-900 selection:bg-red-500 selection:text-red-50"
 >
 	<slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,29 @@
     import {Flower} from 'phosphor-svelte'
 </script>
 
-<div class="mt-96 xl:-ml-16">
+<div class="mt-[600px] ml-5 md:ml-10" >
 	<!-- <div class="rosanita text-9xl font-bold tracking-tight text-red-500">Rosanita</div> -->
 
-    <div class="absolute text-9xl right-0 top-0 opacity-40 rotate-12 hover:rotate-45 transition-transform duration-700">
+    <div class="absolute text-9xl right-3 top-0 opacity-40 rotate-12 blur-[2px] hover:blur-none hover:rotate-45 transition-all duration-700">
         <Flower weight={'thin'}/>
     </div>
 
-    <div class="absolute text-6xl right-28 top-16 opacity-70 rotate-6 hover:rotate-45 transition-transform duration-700">
+    <div class="absolute text-7xl right-28 top-[1em] opacity-70 blur-[1px] hover:blur-none rotate-6 hover:rotate-45 transition-all duration-700">
         <Flower weight={'duotone'}/>
     </div>
 
-    <div class="absolute text-3xl right-32 top-9 rotate-12 hover:rotate-45 transition-transform duration-500">
+    <div class="absolute text-5xl right-20 top-28 rotate-12 hover:rotate-45 transition-transform duration-500">
         <Flower weight={'regular'}/>
     </div>
 
 	<div class="mb-10">Hiii, I'm</div>
-	<div class="rosanita">
+	<div>
 		<HeroAnimated text={'Rosanita'} />
 	</div>
 
 
-	<p class="my-10 text-lg max-w-xs">
+	<p class="my-10 text-lg max-w-sm">
 		I am a young <MovingWord word={'artist'} /> experimenting and having fun. My main sources of inspiration
-		are nature and home. Thanks for stopping by and making a place for me &lt;3
+		are nature and home. Thanks for stopping by &lt;3
 	</p>
 </div>
